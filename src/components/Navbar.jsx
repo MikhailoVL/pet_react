3f927038416e627c225ce82c1917fda6a7a41e8e
LVMykhailo <mykhailo.liutyi@introlab-systems.com>
2021-11-24 13:12:41 +0200

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return(
+        <nav className='nav'>
+            <a>Profile</a>
+            <a>Message</a>
+            <a>News</a>
+            <a>Music</a>
+            <a>Settings</a>
+        </nav>
+    );
+}
+
+export default Navbar
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+const Profile = () => {
+    return(
+          <div className='content'>
+              <div>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM57RyNO5xHejFw7IBohB6m5A3NvWkcRnjwg&usqp=CAU'/>
+              </div>
+
+              <div>
+                  ava + description
+                  <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhZU9b8oKesIgNFYNbXoIXP1VzmP5rvS-v6Q&usqp=CAU'/>
+              </div>
+
+              <div>
+                  my post
+                  <div>
+                      new post
+                  </div>
+                  <div>
+                      post 1
+                  </div>
+                  <div>
+                      post 2
+                  </div>
+              </div>
+
+          </div>
+    );
+}
+
+export default Profile
